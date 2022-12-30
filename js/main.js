68721633c6baf5c2a23f4aba2999aa5714cd6719
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-12-29 20:41:52 -0500

    Rework entire project. Global variables removed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,68 @@
-const displayScreen = document.querySelector('.display');
-const memory = {
-    numArray0: [],
-    numArray1: [],
-    operatorArray0: [],
-};
+
+
+operations();
+function operations() {
+    const memory = {
+        workingArray: [],
+        workingArray1: [],
+        workingArray2: [],
+    };
+    
+    const buttons = document.querySelectorAll('input[type="button"]');
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function() {
+            if((memory['workingArray2'].length > 1 && memory['workingArray1'] 
+            != [] && isNaN(button.value)) || button.value === '=') {
+                let x = parseFloat(memory['workingArray2'][0]);
+                let op = memory['workingArray2'][1];
+                let y = parseFloat(memory['workingArray1'][0]);
+                console.log(typeof x);
+                console.log(op);
+                console.log(typeof y);
+                console.log(operate(x, op, y));
+                return;
+            }
+            if(isNaN(button.value)) {
+                (memory['workingArray1']).push(button.value);
+                memory['workingArray2'] = memory['workingArray1'];
+                console.log(memory['workingArray2']);
+                
+                memory['workingArray'] = [];
+                console.log(memory['workingArray']);
+                return;
+            }
+            (memory['workingArray']).push(button.value);
+            memory['workingArray1'] = (memory['workingArray']).join('').split(' ')
+            console.log(memory['workingArray1']);
+            if(memory['workingArray2'].length > 1 && memory['workingArray1'] 
+            != [] && isNaN(button.value)) {
+                // operate(memory['workingArray2'], memory['workingArray1'])
+                let x = memory['workingArray2'][0];
+                console.log(x);
+            }
+            
+        });
+    });
+    
+    function operate(x, op, y) {
+        switch (op) {
+            case ('+'): 
+                op = add;
+                break;
+            case ('-'): 
+                op = subtract; // could i use returns
+                break;
+            case ('*'): 
+                op = multiply;
+                break;
+            case ('/'): 
+                op = divide;
+                break;
+        }
+
+        return op(x, y);
+    }
+}
 
 function add(x, y) {
     return x + y;
@@ -20,77 +79,5 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+        
 
-function operate(x, op, y) {
-     switch (op) {
-        case ('+'): 
-            op = add;
-            break;
-        case ('-'): 
-            op = subtract;
-            break;
-        case ('*'): 
-            op = multiply;
-            break;
-        case ('/'): 
-            op = divide;
-            break;
-    }
-
-    memory['numArray0'] = [op(parseFloat(x), parseFloat(y))];
-    
-    if((memory['numArray0']) % 1 != 0) {
-        memory['numArray0'] = memory['numArray0'].map(function(value) {
-            return Number(value.toFixed(2));
-        });
-    }
-    
-    
-    displayScreen.textContent = memory['numArray0'];
-    memory['numArray1'] = [];
-    if(memory['operatorArray0'] != []) {
-        console.log('it works');
-        // displayScreen.textContent = memory['numArray0'] memory['operatorArray0'];
-        memory['numArray0'] = (memory['numArray0'] + memory['operatorArray0']).split(' ');
-        displayScreen.textContent = memory['numArray0'].join('');
-        memory['numArray1'] = memory['numArray0'];
-    }
-    disableDotButton('enable');
-}
-
-
-
-
-function display(value) { 
-    memory['numArray0'].push(value);
-
-    memory['numArray1'] = memory['numArray0'].join('').split(' ');
-    displayScreen.textContent = memory['numArray1'].join('');
-    if(value === '.') disableDotButton('disable');
-    if(memory['numArray1'].length > 1) {
-        disableDotButton('enable');
-        if(memory['numArray1'][2].includes('.')) disableDotButton('disable');
-    }
-    if(memory['numArray1'][3]) {
-        memory['operatorArray0'] = ` ${memory['numArray1'][3]} `;
-        operate(...memory['numArray1']);
-    }
-}
-
-function disableDotButton(toggle) {
-    if(toggle === 'disable') {
-        const dotButton = document.getElementById('grid-button-dot');
-        dotButton.style.backgroundColor = '#adff2f';
-        dotButton.disabled = true;
-    } else if (toggle === 'enable') {
-        const dotButton = document.getElementById('grid-button-dot');
-        dotButton.style.backgroundColor = null;
-        dotButton.disabled = false;
-    }
-}
-
-function reset() {
-    memory['numArray0'] = [];
-    displayScreen.textContent = '';
-    disableDotButton('enable');
-}
