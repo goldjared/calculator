1351f8dfeb218fb0abeb0ec47ab29ddbb6928001
Jared Gold <114547220+goldjared@users.noreply.github.com>
2022-12-30 22:58:57 -0500

    Implement dot button disable feature.. Entering . will disable the button for that number entry

--- js/main.js
+++ js/main.js
@@ -18,10 +18,12 @@ function operations() {
                 return;
             }
 
-            if(ops === '' && !isNaN(button.value)) {
+            if(ops === '' && (!isNaN(button.value) || button.value === '.')) {
+                if(ops === '' && button.value === '.') disableDotButton('disable');
                 num1 += button.value;
                 displayTop(num1);
                 console.log('1st if ' +num1);
+                
             }
 
             if(num1 != '' && num2 != '' && ops != '' && button.value === '=') {
@@ -29,23 +31,26 @@ function operations() {
                 displayBottom(num1);
                 num2 = '';
                 ops = '';
+                disableDotButton('enable');
                 console.log('5th if ' +num1);
                 return;
             }
 
-            if(ops != '' && isNaN(button.value)) {
+            if(ops != '' && isNaN(button.value) && button.value != '.') {
                 num1 = operate(parseFloat(num1), parseFloat(num2), ops);
                 displayBottom(num1);
                 ops = button.value;
                 displayTop((num1+ops));
+                disableDotButton('enable');
                 num2 = '';
                 console.log('2nd if ' +num1);
                 return;
             }
 
-            if(isNaN(button.value) && button.value != '=') {
+            if(isNaN(button.value) && button.value != '=' && button.value != '.') {
                 ops = button.value;
                 displayTop((num1+ops));
+                disableDotButton('enable');
                 console.log('3rd if ' +ops);
                 return;
             }
@@ -53,6 +58,7 @@ function operations() {
             if(ops != '' && button.value != '=') {
                 num2 += button.value;
                 displayTop((num1+ops+num2));
+                if(ops != '' && button.value != '=' && button.value === '.') return disableDotButton('disable');
                 console.log('4th if ' +num2);
                 return;
             }
@@ -62,18 +68,30 @@ function operations() {
     function displayTop(...x) {
         const displayTop = document.querySelector('.display-one');
         displayTop.textContent = x;
-
-        
     }
+    
     function displayBottom(x) {
         const displayTop = document.querySelector('.display-two');
         displayTop.textContent = x;
+    }
+
+    function disableDotButton(toggle) {
+        if(toggle === 'disable') {
+            const dotButton = document.getElementById('grid-button-dot');
+            dotButton.style.backgroundColor = '#adff2f';
+            dotButton.disabled = true;
+        } else if (toggle === 'enable') {
+            const dotButton = document.getElementById('grid-button-dot');
+            dotButton.style.backgroundColor = null;
+            dotButton.disabled = false;
+        }
     }
 
     function reset() {
         num1 = '';
         num2 = '';
         ops = '';
+        disableDotButton('enable');
         displayTop('');
         displayBottom('');
     }
